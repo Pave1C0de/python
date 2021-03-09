df6e27c4c987a19d0989c0c39b49fa66b4ab6a3b
Pave1C0de <pave1.p0st@yandex.ru>
2021-03-10 00:08:23 +0300

    trying to add sets in 5th task 5 lesson

--- Sets_5.py
+++ Sets_5.py
@@ -11,3 +11,15 @@ result = [23, 1, 3, 10, 4, 11]
 src = [2, 2, 2, 7, 23, 1, 44, 44, 3, 2, 10, 7, 4, 11]
 src_non_recurring = [el for el in src if src.count(el) == 1] # не очень хорошее решение с count()
 print(src_non_recurring)
+
+
+result = list()
+full_set = set(src)
+for el in src:
+    src_cpy = src.copy()
+    src_cpy.remove(el)
+    cmp_set = set(src_cpy)
+    if full_set != cmp_set:
+        result.append(el)
+print(result)
+
